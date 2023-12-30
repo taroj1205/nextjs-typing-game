99a816fdcfde49c711e6d8ed3abff3b223457ef3
taroj1205 <taroj1205@gmail.com>
2023-12-31 11:09:02 +1300

    fix sign-out page

--- src/app/sign-out/page.tsx
+++ src/app/sign-out/page.tsx
@@ -1,13 +1,21 @@
 import { useClerk } from "@clerk/nextjs"
+import { useLoading } from "@yamada-ui/react";
 import { useRouter } from "next/navigation"
 
 const SignOut = () => {
   const router = useRouter()
   const { signOut } = useClerk();
+  const {screen} = useLoading();
 
-  signOut(() => router.push("/"));
+  screen.start()
+
+  signOut(() => {router.push("/");
+    screen.finish();
+});
 
   return (
     <div>Signing you out...</div>
   )
 }
+
+export default SignOut;
