bd7162286cefad0f887230f6a7076ba120bff908
taroj1205 <taroj1205@gmail.com>
2023-12-31 12:18:36 +1300

    improve sign in and sign up form

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-	return (<div className="flex items-center justify-center"><SignIn /></div>);
+	return (<div className="flex items-center justify-center h-[calc(100svh-40px)] op-0"><SignIn /></div>);
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
 		return (
-			<div className="flex items-center justify-center">
+			<div className="flex items-center justify-center h-[calc(100svh-40px)] top-0">
 				<SignUp />
 			</div>
 		);
