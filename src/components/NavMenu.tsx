9a726bdbb7fdaedf8e203fa6b7f2e5e43b615b46
taroj1205 <taroj1205@gmail.com>
2023-12-31 13:14:20 +1300

    improve navmenu hover item

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -34,10 +34,9 @@ export function NavMenu() {
 	}, [isLoaded, userId]);
 
 	const { page } = useLoading();
-	const {signOut} = useClerk();
+	const { signOut } = useClerk();
 	const router = useRouter();
 
-	
 	const handleSignOut = async () => {
 		try {
 			page.start();
@@ -58,58 +57,72 @@ export function NavMenu() {
 					</NavigationMenuLink>
 				</NavigationMenuItem>
 				{isLoaded && userId ? (
-					<><NavigationMenuItem>
-						<NavigationMenuTrigger>Profile</NavigationMenuTrigger>
-						<NavigationMenuContent>
-							<ul className="grid gap-0 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr] mx-auto">
-								<li
-									className="row-span-4 rounded-lg mb-2 lg:mr-2 lg:mb-0"
-									style={{
-										backgroundImage: `url(${userImage})`,
-										backgroundSize: 'cover',
-										backgroundPosition: 'center'
-									}}>
-									<NavigationMenuLink
-										className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-										href="/profile">
-										<div className="mb-2 mt-4 text-lg font-medium">
-											User Profile
-										</div>
-										<p className="text-sm leading-tight text-muted-foreground">
-											Manage your profile and account settings.
-										</p>
-									</NavigationMenuLink>
-								</li>
-								<ListItem href="/settings" title="Settings" />
-								<ListItem className="cursor-pointer" onClick={() => handleSignOut()} title="Sign Out" />
-							</ul>
-						</NavigationMenuContent>
-					</NavigationMenuItem><NavigationMenuItem>
-							<NavigationMenuTrigger>Stats</NavigationMenuTrigger>
+					<>
+						<NavigationMenuItem>
+							<NavigationMenuTrigger>Profile</NavigationMenuTrigger>
 							<NavigationMenuContent>
 								<ul className="grid gap-0 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr] mx-auto">
-									<li className="row-span-4">
-										<img src={userImage} className="rounded-lg" />
+									<li
+										className="row-span-4 rounded-lg mb-2 lg:mr-2 lg:mb-0"
+										style={{
+											backgroundImage: `url(${userImage})`,
+											backgroundSize: "cover",
+											backgroundPosition: "center",
+										}}>
+										<NavigationMenuLink
+											className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+											href="/profile">
+											<div className="mb-2 mt-4 text-lg font-medium">
+												User Profile
+											</div>
+											<p className="text-sm leading-tight text-muted-foreground">
+												Manage your profile and account settings.
+											</p>
+										</NavigationMenuLink>
 									</li>
-									<ListItem href="/leaderboard" title="Leaderboard" />
-									<ListItem href="/dashboard" title="Dashboard" />
+									<ListItem href="/settings" title="Settings" />
+									<ListItem
+										className="cursor-pointer"
+										onClick={() => handleSignOut()}
+										title="Sign Out"
+									/>
 								</ul>
 							</NavigationMenuContent>
-						</NavigationMenuItem></>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuTrigger>Stats</NavigationMenuTrigger>
+							<NavigationMenuContent>
+								<div className="flex p-4 md:w-[400px] lg:w-[500px] mx-auto">
+									<div className="mr-4">
+										<Link href="/profile">
+											<img src={userImage} className="rounded-lg" />
+										</Link>
+									</div>
+									<ul className="flex-grow">
+										<ListItem href="/leaderboard" title="Leaderboard" />
+										<ListItem href="/dashboard" title="Dashboard" />
+									</ul>
+								</div>
+							</NavigationMenuContent>
+						</NavigationMenuItem>
+					</>
 				) : (
-					<><NavigationMenuItem>
+					<>
+						<NavigationMenuItem>
 							<NavigationMenuLink
 								href="/sign-in"
 								className={navigationMenuTriggerStyle()}>
 								Sign In
 							</NavigationMenuLink>
-						</NavigationMenuItem><NavigationMenuItem>
-								<NavigationMenuLink
-									href="/leaderboard"
-									className={navigationMenuTriggerStyle()}>
-									Leaderboard
-								</NavigationMenuLink>
-							</NavigationMenuItem></>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuLink
+								href="/leaderboard"
+								className={navigationMenuTriggerStyle()}>
+								Leaderboard
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+					</>
 				)}
 			</NavigationMenuList>
 		</NavigationMenu>
